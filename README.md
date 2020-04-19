4ab229b58d4dc4af1c16ab8919e4fc9bea916813
Nicolaj Figaw <figaw@hotmail.com>
2020-04-19 16:19:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JobDSL
+
+Demo resources for the Configuration as Code of Jenkins (for Kubernetes) talk on the 21st of April, for the Jenkins Online Meetup group
---
+++ jobdsl/super-seed.groovy
@@ -0,0 +1,16 @@
+job('seed-job') {
+  scm {
+    git {
+      remote {
+        url ('git@github.com:figaw/configuration-as-code-jenkins-k8s-jobdsl.git')
+        credentials('jenkins-github-ssh')
+      }
+    }
+  }
+  steps {
+    dsl {
+      external('jobdsl/**/*.groovy')
+      removeAction('DELETE')
+    }
+  }
+}
